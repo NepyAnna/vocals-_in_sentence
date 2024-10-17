ace39583b7994c1d4caaba60b0e6293869298f62
NepyAnna <nepyann@gmail.com>
2024-10-17 17:05:55 +0200

    add function getSentence, conect it to button

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
-const inputElement = document.getElementById('sentence_from_browser');
-
+function getSentence() {
+    const inputElement = document.getElementById('sentence_from_browser')
+       return console.log(inputElement.value)
+}
