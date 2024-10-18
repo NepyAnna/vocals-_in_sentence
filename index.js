a68247d0ab375f651e14e69366c1614c3c0999fc
NepyAnna <nepyann@gmail.com>
2024-10-18 16:45:29 +0200

    add few options for Duplicity

--- index.js
+++ index.js
@@ -18,5 +18,18 @@ function searchVocals(sentence) {
 
 function deleteDuplicity(list) {
     const inputElement = document.getElementById('sentence_get_undubpicate').value
-    
+    console.log(inputElement);
+    let uniqArray =[...new Set(inputElement)];
+    /*let uniqArray = [...new Set(inputElement)];
+    Array.from(new Set(inputElement.map(item=>item.trim())));
+    */
+    console.log(uniqArray)
 }
+
+/*Array.from(new Set([1,2,3,4,5,5])) // [1,2,3,4,5]
+[...new Set([1,2,3,4,5,5])]  // [1,2,3,4,5]
+
+let uniqArr = Array.from(new Set(arr.map(item=>item.trim())));
+
+const sortArr = arr.filter((it, index) => index === arr.indexOf(it = it.trim()));
+*/
